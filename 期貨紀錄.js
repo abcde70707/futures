41de3ec7b95afe57dafbaa58262f77d561d3db5e
abcde70707 <74337427+abcde70707@users.noreply.github.com>
2021-12-14 19:51:44 +0800

    Update 期貨紀錄.js

--- 期貨紀錄.js
+++ 期貨紀錄.js
@@ -1,1 +1,21 @@
+function loadjson() {
+  var output = "<tbody><tr>";
+  var jsonurl = "期貨紀錄.json";
+  var jsonurlobj = new XMLHttpRequest();
+  jsonurlobj.open("get", jsonurl);
+  jsonurlobj.send(null);
+  jsonurlobj.onload = function () {
+    if (jsonurlobj.status == 200) {
+      var obj = JSON.parse(jsonurlobj.responseText);
+      for (var i in obj) {
+        output += "<td>" + obj[i].year + "/" + obj[i].month + "/" + obj[i].day + "<td>" + "<td>" + obj[i].point + "<td>" + "<td>" + obj[i].reason + "<td>";
+      }
+    }
+  }
+  output += "<tr><tbody>"
+  document.getElementById("display").innerHTML = output;
+}
+function start() {
+  loadjson();
+}
 window.addEventListener("load", start, false);
