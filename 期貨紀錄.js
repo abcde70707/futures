4d168e7e94a46dd6499e94ae2fc583771efe170c
abcde70707 <74337427+abcde70707@users.noreply.github.com>
2021-12-18 21:11:07 +0800

    Update 期貨紀錄.js

--- 期貨紀錄.js
+++ 期貨紀錄.js
@@ -1,22 +1,200 @@
+var jsonUrl = "https://abcde70707.github.io/futures/期貨紀錄.json";
+//印出全部資料
 function loadjson() {
-  var cors = "https://cors-anywhere.herokuapp.com/";
-  var output = "<tbody><tr>";
-  var jsonurl = cors+"https://abcde70707.github.io/futures/期貨紀錄.json";
-  var jsonurlobj = new XMLHttpRequest();
-  jsonurlobj.open("get", jsonurl);
-  jsonurlobj.send(null);
-  jsonurlobj.onload = function () {
-    if (jsonurlobj.status == 200) {
-      var obj = JSON.parse(JSON.stringify(jsonurlobj.responseText));
-      for (var i in obj) {
-        output += "<td>" + obj[i].year + "/" + obj[i].month + "/" + obj[i].day + "<td>" + "<td>" + obj[i].point + "<td>" + "<td>" + obj[i].reason + "<td>";
+  var content;
+  let countpoint = Number(0);
+  let count = Number(0);
+  $.getJSON(jsonUrl, function (data) {
+
+    for (var i in data) {
+      count++;
+      countpoint += Number(data[i].point);
+      var datamonth;
+      if (Number(data[i].month) < 10) {
+        datamonth = "0" + String(data[i].month);
+      }
+      else {
+        datamonth = String(data[i].month);
+      }
+      var dataday;
+      if (Number(data[i].day) < 10) {
+        dataday = "0" + String(data[i].day);
+      }
+      else {
+        dataday = String(data[i].day);
+      }
+      var dataweak;
+      if (Number(data[i].weak) == 1) {
+        dataweak = "一";
+      }
+      if (Number(data[i].weak) == 2) {
+        dataweak = "二";
+      }
+      if (Number(data[i].weak) == 3) {
+        dataweak = "三";
+      }
+      if (Number(data[i].weak) == 4) {
+        dataweak = "四";
+      }
+      if (Number(data[i].weak) == 5) {
+        dataweak = "五";
+      }
+      if (Number(data[i].point) > 0) {//點數大於0
+        content +=
+          "<tr>" +
+          "<td>" + data[i].year + "/" + datamonth + " / " + dataday + " " + "(" + dataweak + ")" + "</td > " +
+          "<td style='color:#FF0033;'>" + "+" + data[i].point + "</td>" +
+          "<td>" + data[i].reason + "</td>" +
+          "<td>" + "<input type='button' value='刪除'/>" + "</td>" +
+          "</tr>";
+      }
+      else if (Number(data[i].point) < 0) {//點數小於0
+        content +=
+          "<tr>" +
+          "<td>" + data[i].year + "/" + datamonth + " / " + dataday + " " + "(" + dataweak + ")" + "</td > " +
+          "<td style='color:#00CC00;'>" + data[i].point + "</td>" +
+          "<td>" + data[i].reason + "</td>" +
+          "<td>" + "<input type='button' value='刪除'/>" + "</td>" +
+          "</tr>";
+      }
+      else {//點數等於0
+        content +=
+          "<tr>" +
+          "<td>" + data[i].year + "/" + datamonth + " / " + dataday + " " + "(" + dataweak + ")" + "</td > " +
+          "<td style='color:white;'>" + data[i].point + "</td>" +
+          "<td>" + data[i].reason + "</td>" +
+          "<td>" + "<input type='button' value='刪除'/>" + "</td>" +
+          "</tr>";
       }
     }
-  }
-  output += "<tr><tbody>"
-  document.getElementById("display").innerHTML = output;
+    document.getElementById("grandtotal").innerHTML = "累計: " + countpoint + "&nbsp;&nbsp;";
+    document.getElementById("average").innerHTML = "平均: " + (countpoint / count).toFixed(2);
+    $("tbody").empty();
+    $("#display").append(content);
+  });
+}
+//清空資料輸入列
+function clearadd() {
+  document.getElementById("year").value = "";
+  document.getElementById("month").value = "";
+  document.getElementById("day").value = "";
+  document.getElementById("weak").value = "";
+  document.getElementById("point").value = "";
+  document.getElementById("reason").value = "";
+}
+//清空日期查詢列
+function clearsearch() {
+  document.getElementById("years1").value = "";
+  document.getElementById("months1").value = "";
+  document.getElementById("days1").value = "";
+  document.getElementById("years2").value = "";
+  document.getElementById("months2").value = "";
+  document.getElementById("days2").value = "";
+}
+//查詢日期期間
+function search() {
+  var fromyear = document.getElementById("years1").value;
+  var frommonth = document.getElementById("months1").value;
+  var fromday = document.getElementById("days1").value;
+  var toyear = document.getElementById("years2").value;
+  var tomonth = document.getElementById("months2").value;
+  var today = document.getElementById("days2").value;
+  $.getJSON(jsonUrl, function (data) {
+    var content;
+    var count = 0;
+    var countpoint = 0;
+    for (var i in data) {
+      var datamonth;
+      if (Number(data[i].month) < 10) {
+        datamonth = "0" + String(data[i].month);
+      }
+      else {
+        datamonth = String(data[i].month);
+      }
+      var dataday;
+      if (Number(data[i].day) < 10) {
+        dataday = "0" + String(data[i].day);
+      }
+      else {
+        dataday = String(data[i].day);
+      }
+      var dataweak;
+      if (Number(data[i].weak) == 1) {
+        dataweak = "一";
+      }
+      if (Number(data[i].weak) == 2) {
+        dataweak = "二";
+      }
+      if (Number(data[i].weak) == 3) {
+        dataweak = "三";
+      }
+      if (Number(data[i].weak) == 4) {
+        dataweak = "四";
+      }
+      if (Number(data[i].weak) == 5) {
+        dataweak = "五";
+      }
+
+      var inrange = 0
+      var now = Number(data[i].year) * 3000 + Number(data[i].month) * 100 + Number(data[i].day);
+      var low = Number(fromyear) * 3000 + Number(frommonth) * 100 + Number(fromday);
+      var high = Number(toyear) * 3000 + Number(tomonth) * 100 + Number(today);
+      if (Number(now) >= Number(low) && Number(now) <= Number(high)) {
+        count++;
+        countpoint += Number(data[i].point);
+        inrange = 1;
+      }
+      if (Number(inrange) == 1) {
+        if (Number(data[i].point) > 0) {//點數大於0
+          content +=
+            "<tr>" +
+            "<td>" + data[i].year + "/" + datamonth + " / " + dataday + " " + "(" + dataweak + ")" + "</td > " +
+            "<td style='color:#FF0033;'>" + "+" + data[i].point + "</td>" +
+            "<td>" + data[i].reason + "</td>" +
+            "<td>" + "<input type='button' value='刪除'/>" + "</td>" +
+            "</tr>";
+        }
+        else if (Number(data[i].point) < 0) {//點數小於0
+          content +=
+            "<tr>" +
+            "<td>" + data[i].year + "/" + datamonth + " / " + dataday + " " + "(" + dataweak + ")" + "</td > " +
+            "<td style='color:#00CC00;'>" + data[i].point + "</td>" +
+            "<td>" + data[i].reason + "</td>" +
+            "<td>" + "<input type='button' value='刪除'/>" + "</td>" +
+            "</tr>";
+        }
+        else {//點數等於0
+          content +=
+            "<tr>" +
+            "<td>" + data[i].year + "/" + datamonth + " / " + dataday + " " + "(" + dataweak + ")" + "</td > " +
+            "<td>" + data[i].point + "</td>" +
+            "<td>" + data[i].reason + "</td>" +
+            "<td>" + "<input type='button' value='刪除'/>" + "</td>" +
+            "</tr>";
+        }
+      }
+    }
+    if (count == 0)//沒有資料所以不顯示累計和平均
+    {
+      $("span").empty();
+    }
+    else {
+      document.getElementById("grandtotal").innerHTML = "累計: " + countpoint + "&nbsp;&nbsp;";
+      document.getElementById("average").innerHTML = "平均: " + (countpoint / count).toFixed(2);
+    }
+    $("tbody").empty();
+    $("tbody").append(content);
+  });
 }
 function start() {
   loadjson();
+  var searchbutton = document.getElementById("searchbutton");
+  searchbutton.addEventListener("click", search, false);
+  var clearsearchbutton = document.getElementById("clearsearchbutton");
+  clearsearchbutton.addEventListener("click", clearsearch, false);
+  var clearaddbutton = document.getElementById("clearbutton");
+  clearaddbutton.addEventListener("click", clearadd, false);
+  var printallbutton = document.getElementById("printallbutton");
+  printallbutton.addEventListener("click", loadjson, false);
 }
 window.addEventListener("load", start, false);
